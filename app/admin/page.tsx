3fe086a113cb52bd7083ba1e70a6e170f4d67b8d
FELIX P <felix@192.168.2.10>
2026-02-11 23:20:34 +0700

    Update add member feature

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -680,7 +680,7 @@ export default function AdminPage() {
               <div className="bg-[#1a1a24] border border-white/20 rounded-2xl w-full max-w-4xl max-h-[90vh] flex flex-col shadow-2xl">
                   {/* MODAL HEADER */}
                   <div className="p-5 border-b border-white/10 flex justify-between items-center bg-white/5 rounded-t-2xl">
-                      <h2 className="text-xl font-bold text-white flex items-center gap-2"><Megaphone className="text-yellow-400"/> Quản lý Thông báo Marquee</h2>
+                      <h2 className="text-xl font-bold text-white flex items-center gap-2"><Megaphone className="text-yellow-400"/> Quản lý Thông báo </h2>
                       <div className="flex gap-2">
                           <button onClick={handleAddAnnouncement} className="px-4 py-2 rounded-lg text-sm font-bold flex items-center gap-2 bg-yellow-600 hover:bg-yellow-500 text-white shadow-lg transition">
                               <Plus size={18}/> Thêm thông báo mới
